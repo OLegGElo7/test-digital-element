5b5ba26099f8ff27c2b131d9192c1a0a2e70c699
Oleg Sinitsin <wg123@yandex.ru>
2023-09-23 03:12:26 +0500

    endOfTheProject

---
+++ JavaScript/modal.js
@@ -0,0 +1,20 @@
+const modalBtn = document.getElementById("modal__button");
+const modal = document.getElementById("modal");
+const modalClose = document.getElementById("modal__close");
+
+modalBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+});
+
+modalClose.addEventListener("click", () => {
+  modal.style.display = "none";
+  document.body.style.overflow = "visible";
+});
+
+window.addEventListener("click", (Event) => {
+  if (Event.target == modal) {
+    modal.style.display = "none";
+    document.body.style.overflow = "visible";
+  }
+});
